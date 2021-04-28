ba61e0ec48adc266f2374aa7e06690c07ee9791a
Pieter Van Santvliet <pietervs1@hotmail.com>
2021-04-28 16:24:22 +0200

    importing data from csv is working

--- Weather Forecasting/main.py
+++ Weather Forecasting/main.py
@@ -1,1 +1,12 @@
-print('first test')
+import csv
+
+with open('Weather Forecasting/Data/test.csv', newline='') as csvfile:
+    data = list(csv.reader(csvfile))
+
+print(data)
+print(type(data))
+
+## simpler thing that does the same
+# f = open("Weather Forecasting/Data/test.txt", "r")
+# print(f.read())
+# f.close()
