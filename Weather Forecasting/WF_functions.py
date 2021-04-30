5aa1b2a207e9db8211454c857a13d558bc78418a
Pieter Van Santvliet <pietervs1@hotmail.com>
2021-04-30 11:33:28 +0200

    y and x data can be retrieved

---
+++ Weather Forecasting/WF_functions.py
@@ -0,0 +1,4 @@
+## simple data retriever
+# f = open("Weather Forecasting/Data/test.txt", "r")
+# print(f.read())
+# f.close()
--- Weather Forecasting/main.py
+++ Weather Forecasting/main.py
@@ -1,27 +1,41 @@
+print('run started')
+
 import numpy as np
-import matplotlib.pyplot as plt
-import sklearn as sk
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Perceptron
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import balanced_accuracy_score
-from sklearn.neural_network import MLPClassifier
-import sklearn.preprocessing
-import sklearn.impute
-import csv # library for using .csv files
+# import matplotlib.pyplot as plt
+# import sklearn as sk
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import Perceptron
+# from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import balanced_accuracy_score
+# from sklearn.neural_network import MLPClassifier
+# import sklearn.preprocessing
+# import sklearn.impute
+import csv                                              # library for using .csv files
 
 # take the data from test.csv and put it in a list called data
-with open('Weather Forecasting/Data/test.csv', newline='') as csvfile:
-    data = list(csv.reader(csvfile))
+# with open('Weather Forecasting/Data/y_100-data-points.csv', newline='') as csvfile:
+#     data = list(csv.reader(csvfile))
 
-# print(data)
-# print(type(data))
+data = np.genfromtxt('Weather Forecasting/Relevant Data/y_2011-data-points.csv', dtype=float, delimiter=',', skip_header=1)
+y = data[:,1] # only relevant stuff; all rows of column 1
 
-## simpler thing that kinda does the same
-# f = open("Weather Forecasting/Data/test.txt", "r")
-# print(f.read())
-# f.close()
+# print(y)
+# print(y[1:5])
+# print(type(y))
+
+# print('y successfully retrieved')
+
+data = np.genfromtxt('Weather Forecasting/Relevant Data/x_2011-data-points.txt', dtype=float, delimiter=',', skip_header=33)
+x = data[:,3:] # only relevant stuff; all rows of column 3 till end
+
+# print(y)
+# print(x[1:5])
+# print(type(x))
+# print(x[0:2,:])
+
+# print('y successfully retrieved')
 
 
 
-print('success')
+# X = data[:,:-1]
+# y = data[:,-1]
