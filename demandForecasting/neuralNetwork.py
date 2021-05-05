237919bd4c44eff709e4a81af9660fc6c514288b
Pieter Van Santvliet <pietervs1@hotmail.com>
2021-05-05 11:43:27 +0200

    code cleanup and name change

--- demandForecasting/neuralNetwork.py
+++ demandForecasting/neuralNetwork.py
@@ -1,7 +1,8 @@
 '''
 Authors: Aart Rozendaal and Pieter Van Santvliet
-Description: In this script, weather data is used to predict the average capacity factor 
-of wind turbines in Rotterdam. This is done using an Artificial Neural Network.
+Description: In this script, the time and temperature at a certain moment 
+are used to predict the demand of a certain group of houses. This is done 
+using an Artificial Neural Network.
 '''
 
 import os
@@ -11,9 +12,9 @@ import datetime as dt; start_time = dt.datetime.now()
 # display a "Run started" message
 print('Run started at ', start_time.strftime("%X"), '\n')
 
-import functions as f
-import numpy as np
 import sklearn as sk
+import numpy as np
+import numpy.ma as ma
 import sklearn.impute
 from keras.layers import Dense
 from keras.models import Sequential
@@ -24,27 +25,53 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 
-# extracting data from csv file
-try:
-    data = np.genfromtxt('weatherForecasting/relevantData/y_2011-data-points.csv', 
-    dtype=float, delimiter=',', skip_header=1, skip_footer=12)
-    y = data[:,1] # only relevant stuff; all rows of column 1
-except:
-    print('Error while retrieving y'); exit()
-
-# extracting data from txt file
+# extracting input data from txt file
 try:
     data = np.genfromtxt('weatherForecasting/relevantData/x_2011-data-points.txt', 
     dtype=float, delimiter=',', skip_header=33)
-    X = data[:,3:] # only relevant stuff; all rows of column 3 till end
+    X = data[:,[1,2,7,10,21,23]] # only relevant stuff:
+    # select YYYYMMDD (col 1; datum), HH (col 2; hour), T (col 7; temperature), 
+    # SQ (col 10; sunshine duration), R (col 21; rain), O (col 23; storm)
 except:
-    print('Error while retrieving X')
-    exit()
+    print('Error while retrieving input data'); exit()
+
+# we want the weeknumber and daynumber instead of the date
+timeInfo = np.empty((0,2), int)
+for i in X[:,0]:
+    # get the date info from the data file
+    year = int(str(i)[0:4])
+    month = int(str(i)[4:6])
+    day = int(str(i)[6:8])
+    # make a date from the date info
+    time = dt.datetime(year, month, day)
+    # timeInfo will contain the weeknumber (%V) and the daynumber (%u)
+    timeInfo = np.append(timeInfo, np.array([[time.strftime("%V"),time.strftime("%u")]]), axis=0)
+
+# the date-column is replaced by a weeknumber and daynumber column
+X = np.append(timeInfo, np.delete(X,0,1), 1)
+
+# print(X[0:100,:]) # FIXME gekke temperaturen
+
+# extracting output data from csv file
+try:
+    data = np.genfromtxt('demandForecasting/relevantData/Zonnedael - slimme meter dataset - 2013 - Levering.csv', 
+    dtype=float, delimiter=',', skip_header=1, skip_footer=34992)
+    y = data[:,2:-2] # only relevant stuff:
+    # select YYYYMMDD (col 1; datum), HH (col 2; hour), T (col 7; temperature), 
+    # SQ (col 10; sunshine duration), R (col 21; rain), O (col 23; storm)
+except:
+    print('Error while retrieving output data'); exit()
+
+y = y.reshape(-1,4,y.shape[-1]).sum(1)
+
+# from https://stackoverflow.com/q/18689235 # FIXME using pandas
+y = np.where(np.isnan(y), ma.array(y, mask=np.isnan(y)).mean(axis=1)[:, np.newaxis], y)
+y = y.sum(1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=42)
 # f.printSets(X_train, X_test, y_train, y_test) # enable to print set shapes
 
-# checking and handling missing values 
+# checking and handling missing values # FIXME improve NaN handling
 imp = sk.impute.SimpleImputer(missing_values=np.nan, strategy='median')
 X_train = imp.fit_transform(X_train)
 X_test = imp.fit_transform(X_test)
--- weatherForecasting/linearRegression.py
+++ weatherForecasting/linearRegression.py
@@ -1,21 +1,19 @@
-# TODO better file name
 '''
 Authors: Aart Rozendaal and Pieter Van Santvliet
 Description: In this script, weather data is used to predict the average capacity factor 
-of wind turbines in Rotterdam.
+of wind turbines in Rotterdam. This is done using Linear Regression.
 '''
 
 import os; os.system('cls') # clears the command window
-import datetime
+import datetime as dt; start_time = dt.datetime.now()
 # display a "Run started" message
-print("Run started at {}\n".format(datetime.datetime.now().strftime("%X")))
+print('Run started at ', start_time.strftime("%X"), '\n')
 
 import sklearn.impute
 import numpy as np
 import sklearn as sk
 import functions as f
 from sklearn.metrics import mean_squared_error
-from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
@@ -40,10 +38,8 @@ except:
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=None)
 f.printSets(X_train, X_test, y_train, y_test)
 
-# TODO standardization
-
 # checking and handling missing values 
-# FIXME improve NaN handling
+# (standardization and improved NaN handling are missing)
 imp = sk.impute.SimpleImputer(missing_values=np.nan, strategy='median')
 X_train = imp.fit_transform(X_train)
 X_test = imp.fit_transform(X_test)
@@ -58,3 +54,6 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 print("The MSE of this model is: {}".format(mean_squared_error(y_test, y_pred)))
+
+# print the runtime
+print('\nRuntime was', (dt.datetime.now() - start_time).total_seconds(), 'seconds')
