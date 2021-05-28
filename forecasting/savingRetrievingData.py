c0bfc3961727ff549c333fa1fb5614d3e422adbc
Pieter Van Santvliet <pietervs1@hotmail.com>
2021-05-28 12:06:38 +0200

    saving and retrieving data now added

---
+++ forecasting/savingRetrievingData.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+##################################################################
+###################### RETRIEVING DATA FILE ######################
+##################################################################
+
+
+data = np.load('data_V1.npz')
+predSun = data['predSun']
+predWind = data['predWind']
+predDemand = data['predDemand']
+
+print('\nfile consists of:\n', data.files, '\n')
+print('shape sun:\t',predSun.shape)
+print('shape wind:\t',predWind.shape)
+print('shape demand:\t',predDemand.shape)
+
+
+###################################################################
+################### CREATING & SAVING DATA FILE ###################
+###################################################################
+
+
+# # select data
+# predDemand = np.load('predictedDemand_V1.npy')
+# predWind = np.load('predictedWind_V1.npy')
+# weather = np.load('weatherData_V1.npy')
+
+# # to retrieve the original (for this the functions module is needed)
+# # import functions as fs
+# # weather = fs.retrieveWeatherData()
+# # np.save('weatherData_V1', weather)
+
+# def normalizeData(x):
+#     return (x-np.min(x))/(np.max(x)-np.min(x))
+
+# # selecting and scaling wind data
+# predWind = predWind[105192:113951+1] # select 2013
+# predWind = normalizeData(predWind) # normalize the data
+# peakWindPower = 14664 # [W] # peak power of wind generation
+# predWind = peakWindPower*predWind # [Wh/hour] # scale the data
+
+# # selecting and scaling solar data
+# predSun = weather[:,11] # [J/cm2] # select Global radiation
+# predSun = normalizeData(predSun) # normalize the data
+# peakSunPower = 22350 # [W] # peak power of solar generation
+# predSun = peakSunPower*predSun # [Wh/hour] # scale the data
+
+# np.savez('data_V1.npz', predSun=predSun, predWind=predWind, predDemand=predDemand)
