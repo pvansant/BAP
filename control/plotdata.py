a5da134e9bbbef741353e7a1af7565ba0527c01f
aart <aart.m.rozendaal@gmail.com>
2021-06-05 23:54:41 +0200

    commenten en code clean up

--- control/plotdata.py
+++ control/plotdata.py
@@ -1,11 +1,9 @@
 '''
 Authors: Aart Rozendaal and Pieter Van Santvliet
-Description: In this script, the functionality of the controller is programmed
-It used different functions and a loop to control the system.
+Description: In this script, data of the predictions and actual measurements is plotted.
 '''
 
 import os
-
 from numpy.core.function_base import linspace
 from numpy.lib.function_base import append, diff
 os.system('cls') # clears the command window
@@ -30,7 +28,7 @@ predWind = data['predWind']
 predDemand = data['predDemand']
 
 # load actual data
-data1 = np.load('data_original_V2_with-scaling.npz')
+data1 = np.load('data_original_V3.npz')
 sun = predSun
 wind = data1['windOutput']
 demand = data1['demandOutput']
@@ -38,25 +36,25 @@ demand = data1['demandOutput']
 plt.subplot(3,1,1)
 plt.plot(sun, c = 'black', ls = '-') # plot the set point
 plt.plot(predSun, c = '#0C7CBA', ls = '-') # plot the SOC
-
+plt.title("Solar generation data")
 plt.xlabel("Time [hours]")
 plt.ylabel("predicted solar generation [Wh]")
 plt.subplot(3,1,2)
 plt.plot(wind, c = 'black', ls = '-') # plot the set point
 plt.plot(predWind, c = '#0C7CBA', ls = '-') # plot the SOC
-
+plt.title("Wind generation data")
 plt.xlabel("Time [hours]")
 plt.ylabel("predicted wind generation [Wh]")
 plt.subplot(3,1,3)
 plt.plot(demand, c = 'black', ls = '-') # plot the set point
 plt.plot(predDemand, c = '#0C7CBA', ls = '-') # plot the SOC
-
+plt.title("Demand data")
 plt.xlabel("Time [hours]")
 plt.ylabel("predicted demand [Wh]")
-
-plt.show()
-
-
+plt.subplots_adjust(wspace=0.05, hspace=1.5)
 # print the runtime
 print('\nRuntime was', (dt.datetime.now() - start_time).total_seconds(), 'seconds')
 
+# show plot
+
+plt.show()
